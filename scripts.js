b5f11c8f3dea2c310fd07147c6677da20759992d
Gabriella Sinka-Pálinkás <fusewent@gmail.com>
2019-05-13 02:05:19 +0200

    fix

--- scripts.js
+++ scripts.js
@@ -111,7 +111,6 @@ function getExchangeRate(valuta) {
 
                   } finally {
                     updateChart();                  
-                });
                   }
             }
         }
